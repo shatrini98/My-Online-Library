00ee22d4916940c719405ecf6038e806ed1a4b69
shatrini98 <34288599+shatrini98@users.noreply.github.com>
2017-12-05 18:24:02 -0500

    Add files via upload

---
+++ My online library/inc/data.php
@@ -0,0 +1,170 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Naruto: Volume 1",
+	"img" => "img/media/naruto1.jpg",
+    "genre" => "Graphic Novel",
+    "format" => "Paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Masashi Kishimoto",
+
+    ],
+    "publisher" => "Masashi Kishimoto",
+    "isbn" => '1569319006'
+];
+$catalog[102] = [
+    "title" => "Naruto: Volume 8",
+    "img" => "img/media/naruto2.jpg",
+    "genre" => "Graphic Novel",
+    "format" => "paperback",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Masashi Kishimoto"
+    ],
+    "publisher" => "Masashi Kishimoto",
+    "isbn" => ' 1421501244'
+];
+$catalog[103] = [
+    "title" => "Naruto: Volume 11",
+    "img" => "img/media/naruto3.jpg",
+    "genre" => "Graphic Novel",
+    "format" => "paperback",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Masashi Kishimoto",
+
+    ],
+    "publisher" => "Masashi Kishimoto",
+    "isbn" => '1421502410'
+];
+$catalog[104] = [
+    "title" => "The Clean Coder: A Code of Conduct for Professional Programmers",
+    "img" => "img/media/clean_coder.jpg",
+    "genre" => "Tech",
+    "format" => "Audio",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Robert C. Martin"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Superbad",
+    "img" => "img/media/Superbad.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2007,
+    "category" => "Movies",
+    "director" => "Greg Motolla",
+    "writers" => [
+        "seth Rogan",
+        "Evan Goldberg"
+    ],
+    "stars" => [
+        "Jonah Hill",
+        "Michael Cera",
+        "Christopher Mintz-Plasse",
+
+    ]
+];
+$catalog[202] = [
+    "title" => "Pineapple Express",
+    "img" => "img/media/pineappleexpress.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2008,
+    "category" => "Movies",
+    "director" => "David Gordon Green",
+    "writers" => [
+        "Seth Rogan"
+    ],
+    "stars" => [
+        "Seth Rogan",
+        "James Franco",
+        "Gary Cole",
+
+    ]
+];
+$catalog[203] = [
+    "title" => "This Is The End",
+    "img" => "img/media/thisistheend.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2013,
+    "category" => "Movies",
+    "director" => "Seth Rogan",
+    "writers" => [
+        "Seth Rogan",
+        "Evan Goldberg",
+
+    ],
+    "stars" => [
+        "James Franco",
+        "Jonah Hill",
+        "Seth Rogan",
+
+    ]
+];
+$catalog[204] = [
+    "title" => "Youth in Revolt",
+    "img" => "img/media/youthinrevolt.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "Miguel Arteta",
+    "writers" => [
+        "Gustin Nash"
+    ],
+    "stars" => [
+        "Michael Cera",
+        "Portia Doubleday",
+        "Ray Liotta",
+
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Grauduation",
+    "img" => "img/media/graduation.jpg",
+    "genre" => "Hip Hop",
+    "format" => "CD",
+    "year" => 2007,
+    "category" => "Music",
+    "artist" => "Kanye West"
+];
+$catalog[302] = [
+    "title" => "yeezus",
+    "img" => "img/media/yeezus.jpg",
+    "genre" => "Hip Hop",
+    "format" => "CD",
+    "year" => 2013,
+    "category" => "Music",
+    "artist" => "Kanye West"
+];
+$catalog[303] = [
+    "title" => "The Life Of Pablo",
+    "img" => "img/media/thelifeofpablo.jpg",
+    "genre" => "Hip Hop",
+    "format" => "CD",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Kanye West"
+];
+$catalog[304] = [
+    "title" => "The college dropout",
+    "img" => "img/media/thecollegedropout.jpg",
+    "genre" => "Hip Hop",
+    "format" => "CD",
+    "year" => 2008,
+    "category" => "Music",
+    "artist" => "Kanye West"
+];
